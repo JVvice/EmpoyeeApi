cc77301ab97d174742c5bd416ba12e56a04a4789
Javon Spencer <hoop.spencer@gmail.com>
2023-06-05 23:00:36 -0700

    Added HttpGet request for employees

---
+++ EmpoyeeApi/Controllers/EmployeesContoller.cs
@@ -0,0 +1,26 @@
+﻿using EmpoyeeApi.Data;
+using EmpoyeeApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmpoyeeApi.Controllers
+{
+    [ApiController]
+    [Route("api/employees")]
+    public class EmployeesContoller : ControllerBase
+    {
+        private AppDbContext _context;
+        public EmployeesContoller(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            var employees = await _context.Employees.ToListAsync();
+            return Ok(employees);
+        }
+
+    }
+}
